b1b0ffc25625b709027ef4bc443bf365179f2f5e
kameshsr <kameshsr1338@gmail.com>
2026-07-07 18:24:58 +0530

    MOSIP-41105 Removed ip and host column
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_upgrade_scripts/mosip_resident/sql/1.2.0.1_to_1.2.1.0_rollback.sql
+++ db_upgrade_scripts/mosip_resident/sql/1.2.0.1_to_1.2.1.0_rollback.sql
@@ -10,6 +10,12 @@ TO residentuser;
 
 ALTER TABLE resident.resident_session alter column machine_type type varchar(30);
 
+-- Restoring ip_address and host columns
+ALTER TABLE resident.resident_session ADD COLUMN IF NOT EXISTS ip_address character varying(128);
+ALTER TABLE resident.resident_session ADD COLUMN IF NOT EXISTS host character varying(128);
+COMMENT ON COLUMN resident.resident_session.ip_address IS 'The ip_address of device from which the user logged in';
+COMMENT ON COLUMN resident.resident_session.host IS 'The host of the site';
+
 DROP INDEX IF EXISTS idx_resident_user_actions_ida_token;
 
 REVOKE SELECT, INSERT, REFERENCES, UPDATE, DELETE
