ce8147de3862c2238bbdc6543303d7ed9111a368
kameshsr <kameshsr1338@gmail.com>
2026-07-08 11:59:50 +0530

    MOSIP-41105 corrected upgrade script
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_upgrade_scripts/mosip_resident/sql/1.2.0.1_to_1.2.1.0_upgrade.sql
+++ db_upgrade_scripts/mosip_resident/sql/1.2.0.1_to_1.2.1.0_upgrade.sql
@@ -9,7 +9,6 @@ ON resident.resident_grievance_ticket
 TO residentuser;
 
 ALTER TABLE resident.resident_session alter column machine_type type varchar(100);
-
 -- Adding index to ida_token column
 CREATE INDEX idx_resident_user_actions_ida_token ON resident.resident_user_actions (ida_token);
 GRANT SELECT, INSERT, REFERENCES, UPDATE, DELETE
